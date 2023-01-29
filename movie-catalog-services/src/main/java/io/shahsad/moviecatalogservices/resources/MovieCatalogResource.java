3005e6b1cf24b455ff5374cbdf961442dc0767b2
shahsad-m-zluri <shahsad.m@zluri.com>
2023-01-29 21:15:26 +0530

    connecting_3_apps

--- movie-catalog-services/src/main/java/io/shahsad/moviecatalogservices/resources/MovieCatalogResource.java
+++ movie-catalog-services/src/main/java/io/shahsad/moviecatalogservices/resources/MovieCatalogResource.java
@@ -1,12 +1,17 @@
 package io.shahsad.moviecatalogservices.resources;
 
 import io.shahsad.moviecatalogservices.models.CatalogItem;
+import io.shahsad.moviecatalogservices.models.Movie;
+import io.shahsad.moviecatalogservices.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -16,6 +21,22 @@ public class MovieCatalogResource {
 
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(new CatalogItem("The man from earth","Test",5));
+        RestTemplate restTemplate = new RestTemplate();
+        //restTemplate.getForObject("http://localhost:8082/movies/o", Movie.class);
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("test1",4),
+                new Rating("test2",3),
+                new Rating("test3",5)
+        );
+
+        return ratings.stream().map(rating -> {
+                    Movie movie= restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                    return new CatalogItem(movie.getName(), "Test", rating.getRating());
+                } )
+                .collect(Collectors.toList());
+
+        //return Collections.singletonList(new CatalogItem("The man from earth","Test",5));
     }
 }
