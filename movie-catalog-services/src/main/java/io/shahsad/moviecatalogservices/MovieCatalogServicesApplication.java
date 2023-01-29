d57099d6beaf0677028742ea637f6a9f7434cfb9
shahsad-m-zluri <shahsad.m@zluri.com>
2023-01-29 19:06:33 +0530

    init2

---
+++ movie-catalog-services/src/main/java/io/shahsad/moviecatalogservices/MovieCatalogServicesApplication.java
@@ -0,0 +1,13 @@
+package io.shahsad.moviecatalogservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServicesApplication.class, args);
+	}
+
+}
---
+++ movie-catalog-services/src/test/java/io/shahsad/moviecatalogservices/MovieCatalogServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shahsad.moviecatalogservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
