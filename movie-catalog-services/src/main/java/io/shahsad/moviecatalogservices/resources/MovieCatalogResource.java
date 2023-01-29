132144a08e4b876b92e9ad42975784b0fbc58579
shahsad-m-zluri <shahsad.m@zluri.com>
2023-01-29 21:26:51 +0530

    adding_BEAN_AUTOWIRED

--- movie-catalog-services/src/main/java/io/shahsad/moviecatalogservices/resources/MovieCatalogResource.java
+++ movie-catalog-services/src/main/java/io/shahsad/moviecatalogservices/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package io.shahsad.moviecatalogservices.resources;
 import io.shahsad.moviecatalogservices.models.CatalogItem;
 import io.shahsad.moviecatalogservices.models.Movie;
 import io.shahsad.moviecatalogservices.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,15 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping ("/catalog")
 public class MovieCatalogResource {
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/{userId}")
 
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        RestTemplate restTemplate = new RestTemplate();
+        /// Removing since bean will inject the instance, Autowire is the consumer
+        ///Bean and autowire are used to create multiple instance for rest, all use the same model
+        //RestTemplate restTemplate = new RestTemplate();
         //restTemplate.getForObject("http://localhost:8082/movies/o", Movie.class);
 
         List<Rating> ratings = Arrays.asList(
